d3cbb9a6cc99587286bf5e46b5388909d2f98786
ErikaArci99 <erikaarci1999@outlook.it>
2025-03-31 17:12:32 +0200

    [UPD] creata funzione card e ciclo array

--- js/script.js
+++ js/script.js
@@ -36,3 +36,25 @@ const teamMembers = [
       img: "img/female3.png"
     }
   ];
+
+
+// funzione che mi crea la singola card del membro
+const createMemberCard = (member) => {
+    const card = `<div class="col-12 col-md-6 col-lg-4">
+                <div class="team-card bg-black d-flex">
+                    <div class="card-img width-img">
+                        <img class="img-fluid" src="./${member.img}" alt="">
+                    </div>
+                    <div class="card-text d-flex flex-column justify-content-between my-2 mx-3">
+                        <span class="text-light fs-4 fw-bold">${member.name}</span>
+                        <span class="text-light">${member.role}</span>
+                        <span class="text-info">${member.email}</span>
+                    </div>
+                </div>
+            </div>`;
+}
+
+// ciclo array members
+for (let i = 0; i < teamMembers.length; i++){
+    createMemberCard(teamMembers[i])
+}
