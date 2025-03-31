1107a349645224e8e592edc2cbedcf2cb0033a5c
ErikaArci99 <erikaarci1999@outlook.it>
2025-03-31 17:18:10 +0200

    [DONE] esercizio terminato gia con il 1 bonus perchè è responsive

--- js/script.js
+++ js/script.js
@@ -1,46 +1,46 @@
 const teamMembers = [
     {
-      name: "Marco Bianchi",
-      role: "Designer",
-      email: "marcobianchi@team.com",
-      img: "img/male1.png"
+        name: "Marco Bianchi",
+        role: "Designer",
+        email: "marcobianchi@team.com",
+        img: "img/male1.png"
     },
     {
-      name: "Laura Rossi",
-      role: "Front-end Developer",
-      email: "laurarossi@team.com",
-      img: "img/female1.png"
+        name: "Laura Rossi",
+        role: "Front-end Developer",
+        email: "laurarossi@team.com",
+        img: "img/female1.png"
     },
     {
-      name: "Giorgio Verdi",
-      role: "Back-end Developer",
-      email: "giorgioverdi@team.com",
-      img: "img/male2.png"
+        name: "Giorgio Verdi",
+        role: "Back-end Developer",
+        email: "giorgioverdi@team.com",
+        img: "img/male2.png"
     },
     {
-      name: "Marta Ipsum",
-      role: "SEO Specialist",
-      email: "martarossi@team.com",
-      img: "img/female2.png"
+        name: "Marta Ipsum",
+        role: "SEO Specialist",
+        email: "martarossi@team.com",
+        img: "img/female2.png"
     },
     {
-      name: "Roberto Lorem",
-      role: "SEO Specialist",
-      email: "robertolorem@team.com",
-      img: "img/male3.png"
+        name: "Roberto Lorem",
+        role: "SEO Specialist",
+        email: "robertolorem@team.com",
+        img: "img/male3.png"
     },
     {
-      name: "Daniela Amet",
-      role: "Analyst",
-      email: "danielaamet@team.com",
-      img: "img/female3.png"
+        name: "Daniela Amet",
+        role: "Analyst",
+        email: "danielaamet@team.com",
+        img: "img/female3.png"
     }
-  ];
+];
 
 
 // funzione che mi crea la singola card del membro
 const createMemberCard = (member) => {
-    const card = `<div class="col-12 col-md-6 col-lg-4">
+    const card = `<div class="col-12 col-md-6 col-lg-4 mt-4">
                 <div class="team-card bg-black d-flex">
                     <div class="card-img width-img">
                         <img class="img-fluid" src="./${member.img}" alt="">
@@ -52,9 +52,10 @@ const createMemberCard = (member) => {
                     </div>
                 </div>
             </div>`;
+    document.getElementById('team-members').innerHTML += card;
 }
 
 // ciclo array members
-for (let i = 0; i < teamMembers.length; i++){
+for (let i = 0; i < teamMembers.length; i++) {
     createMemberCard(teamMembers[i])
 }
